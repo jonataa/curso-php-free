bd9bb1afa5311bbdbe46b3069c756b9ab46eb304
Thiago Mascarenhas Oliveira <thiago_si@ymail.com>
2015-08-29 00:04:28 -0300

    Segundo exercício para aula1

---
+++ exercicios/aula1/extrato_grid.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Extrato Grid</title>
+  </head>
+  <body>
+    <center><h1>Extrato Bancário</h1></center>
+    <table border="1" width="500" align="center" celspacing="0" celpadding="0">
+      <thead>
+        <th>Data</th>
+        <th>Descrição</th>
+        <th>Valor</th>
+      </thead>
+      <!--
+      Paleta de cores:
+      * #66c169 : Verde para valor Positivo
+      * #f15e5e : Vermelho para valor negativo
+      -->
+      <tbody align="center">
+        <tr style="background-color: #66c169;">
+          <td colspan="2" align="right"><i><strong>Saldo Anterior</strong><i></td>
+          <td align="right">R$ 5000,00</td>
+        </tr>
+        <tr style="background-color: #f15e5e;">
+          <td>28/08/2015</td>
+          <td>Transferência Online</td>
+          <td>R$ -5500,65</td>
+        </tr>
+        <tr style="background-color: #f15e5e;">
+          <td>28/08/2015</td>
+          <td>Transferência Online</td>
+          <td>R$ -5500,65</td>
+        </tr>
+        <tr style="background-color: #f15e5e;">
+          <td>28/08/2015</td>
+          <td>Transferência Online</td>
+          <td>R$ -5500,65</td>
+        </tr>
+        <tr style="background-color: #f15e5e;">
+          <td>28/08/2015</td>
+          <td>Transferência Online</td>
+          <td>R$ -5500,65</td>
+        </tr>
+        <tr style="background-color: #f15e5e;">
+          <td>28/08/2015</td>
+          <td>Transferência Online</td>
+          <td>R$ -5500,65</td>
+        </tr>
+        <tr style="background-color: #f15e5e;">
+          <td>28/08/2015</td>
+          <td>Transferência Online</td>
+          <td>R$ -5500,65</td>
+        </tr>
+        <tr style="background-color: #66c169;">
+          <td>28/08/2015</td>
+          <td>Transferência Online</td>
+          <td>R$ 5500,65</td>
+        </tr>
+        <tr style="background-color: #f15e5e;">
+          <td colspan="2" align="right"><i><b>Saldo Final</b><i></td>
+          <td align="right">R$ -22503,25</td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
